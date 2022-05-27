bbaba9588fe0a7cf4f18474bd9f27f7dc3b37b54
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-05-27 08:44:33 -0600

    start prep for deployment

--- make_map.py
+++ make_map.py
@@ -19,7 +19,7 @@ def make_map(restaurants):
                                         <h4 class="card-title fw-bold" style="text-shadow: #cc9a07 0 1px 1px;">{1}</h4>
                                         <h4 class="card-title" style="color: green; text-shadow: #a3cfbc 1px 0 3px;">{2}</h4>
                                         <h6 class="card-text" style="font-style: italic;">{3}</h6>
-                                        <h4 style="color: #f4c109; font-weight:bold; text-shadow: #274298 0 1px 1px;">{5} ⭐️ 
+                                        <h4 style="color: #f4c109; font-weight:bold; text-shadow: #172c65 0 1px 1px;">{5} ⭐️ 
                                         <span style="font-style:italic; font-size: smaller;"> ({6})</span></h4>
                                         <a type="button" href="{4}" class="btn btn-primary btn-sm" style="color:white;">See Details</a>
                                 </div>
